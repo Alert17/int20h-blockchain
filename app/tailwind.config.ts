a923d57ad1765c186df34d5d24e7d40ed77c61c4
Yurii Klymenko <yurii@systemsbyai.com>
2025-03-01 17:05:04 +0200

    feat: login page

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,18 +1,26 @@
 import type { Config } from 'tailwindcss';
 import daisyui from 'daisyui';
 
-export default {
+const config: Config = {
     darkMode: ['class'],
     content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   daisyui: {
     themes: ['dracula', 'forest', 'dark', 'halloween'],
   },
   plugins: [daisyui, require("tailwindcss-animate")],
     theme: {
+    	container: {
+    		center: true,
+    		padding: "2rem",
+    		screens: {
+    			"2xl": "1400px",
+    		},
+    	},
     	extend: {
     		borderRadius: {
     			lg: 'var(--radius)',
@@ -20,16 +28,11 @@ export default {
     			sm: 'calc(var(--radius) - 4px)'
     		},
     		colors: {
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
     			background: 'hsl(var(--background))',
     			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
     			primary: {
     				DEFAULT: 'hsl(var(--primary))',
     				foreground: 'hsl(var(--primary-foreground))'
@@ -37,6 +40,10 @@ export default {
     			secondary: {
     				DEFAULT: 'hsl(var(--secondary))',
     				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
     			},
     			muted: {
     				DEFAULT: 'hsl(var(--muted))',
@@ -46,13 +53,14 @@ export default {
     				DEFAULT: 'hsl(var(--accent))',
     				foreground: 'hsl(var(--accent-foreground))'
     			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
     			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
     			chart: {
     				'1': 'hsl(var(--chart-1))',
     				'2': 'hsl(var(--chart-2))',
@@ -60,7 +68,18 @@ export default {
     				'4': 'hsl(var(--chart-4))',
     				'5': 'hsl(var(--chart-5))'
     			}
+    		},
+    		keyframes: {
+    			"fade-in": {
+    				"0%": { opacity: "0" },
+    				"100%": { opacity: "1" },
+    			},
+    		},
+    		animation: {
+    			"fade-in": "fade-in 0.5s ease-in-out",
     		}
     	}
     }
 } satisfies Config;
+
+export default config;
