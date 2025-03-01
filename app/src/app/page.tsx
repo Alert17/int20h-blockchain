5f4f4d27058f2a941f66ab2afceda4a74f1b3504
Vlad-Makarenko <makrenkovladok@gmail.com>
2025-03-01 15:46:15 +0200

    initialize Next.js project with Tailwind CSS and RainbowKit integration

---
+++ app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from '../components/Hero';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Hero />
+    </div>
+  );
+}
---
+++ app/tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  daisyui: {
+    themes: ['dracula', 'forest', 'dark', 'halloween'],
+  },
+  plugins: [daisyui],
+} satisfies Config;
