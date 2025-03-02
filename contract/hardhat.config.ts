311109f777e63f97b63856802485d8d34246b085
Nikita Zakusilov <coren7986@gmail.com>
2025-03-02 09:32:43 +0200

    deploy on mainnets

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -21,6 +21,26 @@ const config: HardhatUserConfig = {
 			url: "https://eth-sepolia.api.onfinality.io/public",
 			accounts: [process.env.PRIVATE_KEY || ""],
 		},
+		ethereum: {
+			url: "https://eth.blockrazor.xyz",
+			accounts: [process.env.PRIVATE_KEY || ""],
+			chainId: 1,
+		},
+		polygon: {
+			url: "https://polygon-rpc.com",
+			accounts: [process.env.PRIVATE_KEY || ""],
+			chainId: 137,
+		},
+		arbitrum: {
+			url: "https://arb1.arbitrum.io/rpc",
+			accounts: [process.env.PRIVATE_KEY || ""],
+			chainId: 42161,
+		},
+		base: {
+			url: "https://mainnet.base.org",
+			accounts: [process.env.PRIVATE_KEY || ""],
+			chainId: 8453,
+		},
 	},
 };
 
